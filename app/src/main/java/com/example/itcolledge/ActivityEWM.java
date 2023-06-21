d43d74fafdf9f25b3893fb05f8c1b5e4fcad6c23
okamodgi <nadakupnada@gmail.com>
2023-06-21 03:12:10 +0300

    ec

--- app/src/main/java/com/example/itcolledge/ActivityEWM.java
+++ app/src/main/java/com/example/itcolledge/ActivityEWM.java
@@ -15,8 +15,7 @@ public class ActivityEWM extends AppCompatActivity {
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
         TextView text = findViewById(R.id.txt);
-        TextView text2 = findViewById(R.id.txt2);
         text.setText(bundle.getString("Key"));
-        text2.setText(bundle.getString("Key2"));
+
     }
 }
